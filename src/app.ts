f38510382f221a86adb0441b6d8b7f3cdb76458e
Sowparna Shome <soupornoshome@gmail.com>
2025-08-14 23:48:17 +0530

    basic structure for vercel hosting of api

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Your express application configuration
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-// Listen on the specified port; note that Vercel provides a port automatically
 app.listen(port, () => {
   console.log(`Server running on port ${port} http://localhost:${port}`);
 });
