9641290b3d9eef635fc6b8d41f975da926c62455
Sowparna Shome <soupornoshome@gmail.com>
2025-08-15 00:36:10 +0530

    connection add

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import dotenv from "dotenv";
 import config from "./environment/config";
+import connectToDatabase from "./db/connect.mongo";
 
 const app = express();
 const port = config.PORT || 3000;
@@ -9,6 +9,9 @@ const port = config.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+connectToDatabase()
+  .then(() => console.log("✅ Connected to database"))
+  .catch((error) => console.log("❌ Database connection Failed with ", error));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port} http://localhost:${port}`);
