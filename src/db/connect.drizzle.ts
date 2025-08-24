f909ef1dba019e11fec087cb17e93bf72537777c
Sowparna Shome <soupornoshome@gmail.com>
2025-08-24 21:46:40 +0530

    export add

--- src/db/connect.drizzle.ts
+++ src/db/connect.drizzle.ts
@@ -3,4 +3,4 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import config from "../environment/config";
 
 
-const db = drizzle(config.DATABASE_URL);
+export const db = drizzle(config.DATABASE_URL);
